e29e7a54db2a30bbaa984ee7a87e5be9bfd3bb73
old2212 <o.lediberder@gmail.com>
2022-10-25 18:04:54 +0200

    first question with piechart done

--- graph_streamlit.py
+++ graph_streamlit.py
@@ -2,7 +2,8 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
+
+
 
 #connect to the SQL database
 connection = sqlite3.connect('bce.db')
@@ -30,12 +31,20 @@ df_Q1T2.rename(columns = {'Code':'JuridicalForm'}, inplace = True)
 
 #Merge
 df_Q1 = pd.merge(df_Q1T1, df_Q1T2, on='JuridicalForm')
-df_Q1 = df_Q1["Description"]
+df_Q1 = df_Q1[["Description"]]
+df_Q1["Count"]= df_Q1.groupby("Description")["Description"].transform("count")
+df_Q1 = df_Q1.drop_duplicates()
+df_Q1['percent'] = (df_Q1['Count'] / df_Q1['Count'].sum()) * 100
+df_Q1 = df_Q1[["Description", "percent"]]
 
-pie_chart = px.pie(df_Q1,
-                   title="Juridical Form",
-                   values="Description",
-                   names="Description")
 
-Piechart_Jurid = st.plotly_chart(pie_chart)
-Piechart_Jurid
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = df_Q1["Description"]
+sizes = df_Q1["percent"]
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)
